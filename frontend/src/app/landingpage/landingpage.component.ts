5b4ce6dfbdf10e9a97d7bf4071933374e80d1318
awsedx <jason@scheleg.de>
2024-03-11 16:46:31 +0100

    added landing page

--- frontend/src/app/landingpage/landingpage.component.ts
+++ frontend/src/app/landingpage/landingpage.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
